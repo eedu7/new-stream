931de0337c1360799e6cf31177c1a5d4cef327cb
eedu7 <mueedahmad067@gmail.com>
2025-04-09 13:58:59 +0500

    Added the SearchInput style and also input and search button

--- src/modules/home/ui/components/home-navbar/search-input.tsx
+++ src/modules/home/ui/components/home-navbar/search-input.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { SearchIcon } from "lucide-react";
 
 export const SearchInput = () => {
-    return <div>SearchInput</div>;
+    // TODO: Add search functionality
+    return (
+        <form className="flex w-full max-w-[600px]">
+            <div className="relative w-full">
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    className="w-full rounded-l-full border py-2 pr-12 pl-4 focus:border-blue-500 focus:outline-none"
+                />
+                {/* TODO: add remove search button */}
+            </div>
+            <button className="rounded-r-full border-l-0 bg-gray-100 px-5 py-2.5 hover:bg-gray-200 disabled:cursor-not-allowed disabled:opacity-50">
+                <SearchIcon className="size-5" />
+            </button>
+        </form>
+    );
 };
