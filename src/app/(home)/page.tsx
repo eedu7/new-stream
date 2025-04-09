6fc457e1097c998080855603f9ae7ab4b66d6953
eedu7 <mueedahmad067@gmail.com>
2025-04-09 18:05:31 +0500

    tested the tRPC client and server and working fine

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-    return <p>I will load videos in the future</p>;
+// <-- hooks can only be used in client components
+import { trpc } from "@/trpc/server";
+
+export default async function Home() {
+    const data = await trpc.hello({
+        text: "- NewStream 0134",
+    });
+    return <p>Client component says: {data?.greeting}</p>;
 }
