9ff3450984d592b8a0909171e07e1f4e839cba42
eedu7 <mueedahmad067@gmail.com>
2025-04-09 14:33:04 +0500

    Added the PersonalSection

--- src/modules/home/ui/components/home-sidebar/index.tsx
+++ src/modules/home/ui/components/home-sidebar/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
 import { MainSection } from "@/modules/home/ui/components/home-sidebar/main-section";
+import { Separator } from "@/components/ui/separator";
+import { PersonalSection } from "@/modules/home/ui/components/home-sidebar/personal-section";
 
 export const HomeSidebar = () => {
     return (
         <Sidebar className="z-40 border-none pt-16">
             <SidebarContent className="bg-background">
                 <MainSection />
+                <Separator />
+                <PersonalSection />
             </SidebarContent>
         </Sidebar>
     );
