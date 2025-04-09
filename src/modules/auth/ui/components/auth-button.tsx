67252fec30d17f869d6a928f6c4efba249d2cddf
eedu7 <mueedahmad067@gmail.com>
2025-04-09 14:06:10 +0500

    styled the auth-button

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -3,8 +3,12 @@ import { Button } from "@/components/ui/button";
 import { UserCircleIcon } from "lucide-react";
 
 export const AuthButton = () => {
+    // TODO: Add different auth states
     return (
-        <Button>
+        <Button
+            variant="outline"
+            className="cursor-pointer rounded-full border-blue-500/2 px-4 py-2 text-sm font-medium text-blue-600 shadow-none hover:text-blue-500/20"
+        >
             <UserCircleIcon />
             Sign in
         </Button>
