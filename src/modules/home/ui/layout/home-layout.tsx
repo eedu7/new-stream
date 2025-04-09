01bc87698d77d372a2dfce6eb9bc723573063ca9
eedu7 <mueedahmad067@gmail.com>
2025-04-09 14:10:10 +0500

    Fix the layout of HomeLayout and imported and created the HomeSidebar :

--- src/modules/home/ui/layout/home-layout.tsx
+++ src/modules/home/ui/layout/home-layout.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { HomeNavbar } from "@/modules/home/ui/components/home-navbar";
+import { HomeSidebar } from "@/modules/home/ui/components/home-sidebar";
 
 export const HomeLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <SidebarProvider>
             <div>
                 <HomeNavbar />
-            </div>
-            <div className="flex min-h-screen pt-[4rem]">
-                <main className="flex-1 overflow-y-auto">{children}</main>
+                <div className="flex min-h-screen pt-[4rem]">
+                    <HomeSidebar />
+                    <main className="flex-1 overflow-y-auto">{children}</main>
+                </div>
             </div>
         </SidebarProvider>
     );
