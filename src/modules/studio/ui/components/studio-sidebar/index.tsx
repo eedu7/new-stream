c66b3f323d561d1a6252c7f55bead0d7485e0352
eedu7 <mueedahmad067@gmail.com>
2025-04-10 16:27:42 +0500

    Customized the sidebar for the studio

--- src/modules/studio/ui/components/studio-sidebar/index.tsx
+++ src/modules/studio/ui/components/studio-sidebar/index.tsx
@@ -1,19 +1,56 @@
+"use client";
+
 import React from "react";
-import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
-import { MainSection } from "@/modules/home/ui/components/home-sidebar/main-section";
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+import { LogOutIcon, VideoIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
 import { Separator } from "@/components/ui/separator";
-import { PersonalSection } from "@/modules/home/ui/components/home-sidebar/personal-section";
 
 export const StudioSidebar = () => {
+    const pathname = usePathname();
+
     return (
         <Sidebar
-            className="z-40 border-none pt-16"
+            className="z-40 pt-16"
             collapsible="icon"
         >
             <SidebarContent className="bg-background">
-                <MainSection />
-                <Separator />
-                <PersonalSection />
+                <SidebarGroup>
+                    <SidebarMenu>
+                        <SidebarMenuItem>
+                            <SidebarMenuButton
+                                tooltip="Exit studio"
+                                asChild
+                                isActive={pathname === "/studio"}
+                            >
+                                <Link href="/studio">
+                                    <VideoIcon className="size-5" />
+                                    <span className="text-sm">Content</span>
+                                </Link>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                        <Separator />
+                        <SidebarMenuItem>
+                            <SidebarMenuButton
+                                tooltip="Exit studio"
+                                asChild
+                            >
+                                <Link href="/">
+                                    <LogOutIcon className="size-5" />
+                                    <span className="text-sm">Exit studio</span>
+                                </Link>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    </SidebarMenu>
+                </SidebarGroup>
             </SidebarContent>
         </Sidebar>
     );
