3d7c4f2fc6980bd5f15b2bd1419cc83f8c9f9de8
eedu7 <mueedahmad067@gmail.com>
2025-04-10 12:37:43 +0500

    Added the categoriesProcedure to the _app

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,18 +1,10 @@
-import { z } from "zod";
-import { createTRPCRouter, protectedProcedure } from "../init";
+// export type definition of API
+
+import { createTRPCRouter } from "@/trpc/init";
+import { categoriesRouter } from "@/modules/categories/server/procedures";
 
 export const appRouter = createTRPCRouter({
-    hello: protectedProcedure
-        .input(
-            z.object({
-                text: z.string(),
-            }),
-        )
-        .query(async (opts) => {
-            return {
-                greeting: `hello ${opts.input.text}`,
-            };
-        }),
+    categories: categoriesRouter,
 });
-// export type definition of API
+
 export type AppRouter = typeof appRouter;
