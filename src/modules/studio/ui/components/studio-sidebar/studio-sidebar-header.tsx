5357d62ce1d54b13a0551ff5005d0874ca224fac
eedu7 <mueedahmad067@gmail.com>
2025-04-10 17:05:45 +0500

    Added the sidebar collapsed menu button and skeleton

--- src/modules/studio/ui/components/studio-sidebar/studio-sidebar-header.tsx
+++ src/modules/studio/ui/components/studio-sidebar/studio-sidebar-header.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { useUser } from "@clerk/nextjs";
-import { SidebarHeader } from "@/components/ui/sidebar";
+import { SidebarHeader, SidebarMenuButton, SidebarMenuItem, useSidebar } from "@/components/ui/sidebar";
 import Link from "next/link";
 import { UserAvatar } from "@/components/user-avatar";
 import { Skeleton } from "@/components/ui/skeleton";
@@ -9,16 +9,39 @@ import { Skeleton } from "@/components/ui/skeleton";
 export const StudioSidebarHeader = () => {
     const { user } = useUser();
 
-    if (!user)
+    const { state } = useSidebar();
+
+    if (!user) {
         return (
             <SidebarHeader className="flex items-center justify-center pb-4">
                 <Skeleton className="size-[112px] rounded-full" />
-                <div className="mt-2 flex flex-col items-center gap-y-1">
+                <div className="mt-2 flex flex-col items-center gap-y-2">
                     <Skeleton className="h-4 w-[80px]" />
                     <Skeleton className="h-4 w-[100px]" />
                 </div>
             </SidebarHeader>
         );
+    }
+
+    if (state === "collapsed") {
+        return (
+            <SidebarMenuItem>
+                <SidebarMenuButton
+                    tooltip="Your profile"
+                    asChild
+                >
+                    <Link href={"/users/current"}>
+                        <UserAvatar
+                            imageUrl={user.imageUrl}
+                            name={user.fullName ?? "User"}
+                            size="xs"
+                        />
+                        <span className="text-sm">Your Profile</span>
+                    </Link>
+                </SidebarMenuButton>
+            </SidebarMenuItem>
+        );
+    }
 
     return (
         <SidebarHeader className="flex items-center justify-center pb-4">
