0dd1242051f1d0c71261ee0220e1089d7cfa964b
eedu7 <mueedahmad067@gmail.com>
2025-04-10 17:00:54 +0500

    Added the skeleton for the UserAvatar

--- src/modules/studio/ui/components/studio-sidebar/studio-sidebar-header.tsx
+++ src/modules/studio/ui/components/studio-sidebar/studio-sidebar-header.tsx
@@ -4,11 +4,21 @@ import { useUser } from "@clerk/nextjs";
 import { SidebarHeader } from "@/components/ui/sidebar";
 import Link from "next/link";
 import { UserAvatar } from "@/components/user-avatar";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const StudioSidebarHeader = () => {
     const { user } = useUser();
 
-    if (!user) return null;
+    if (!user)
+        return (
+            <SidebarHeader className="flex items-center justify-center pb-4">
+                <Skeleton className="size-[112px] rounded-full" />
+                <div className="mt-2 flex flex-col items-center gap-y-1">
+                    <Skeleton className="h-4 w-[80px]" />
+                    <Skeleton className="h-4 w-[100px]" />
+                </div>
+            </SidebarHeader>
+        );
 
     return (
         <SidebarHeader className="flex items-center justify-center pb-4">
@@ -19,7 +29,7 @@ export const StudioSidebarHeader = () => {
                     className="size-[112px] transition-opacity hover:opacity-80"
                 />
             </Link>
-            <div className="mt-2 flex flex-col items-center">
+            <div className="mt-2 flex flex-col items-center gap-y-1">
                 <p className="text-sm font-medium">Your profile</p>
                 <p className="text-muted-foreground text-xs">{user?.fullName}</p>
             </div>
