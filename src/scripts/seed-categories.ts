c86760f684ea338611eaa21f683cfa55c521e696
eedu7 <mueedahmad067@gmail.com>
2025-04-10 12:34:08 +0500

    Added the seeded categories

--- src/scripts/seed-categories.ts
+++ src/scripts/seed-categories.ts
@@ -1,1 +1,42 @@
 // TODO: Create a script to seed categories
+
+import { db } from "@/db";
+import { categories } from "@/db/schema";
+
+const categoryNames: string[] = [
+    "AI",
+    "Cars and vehicles",
+    "Comedy",
+    "Education",
+    "Gaming",
+    "Entertainment",
+    "Film and animation",
+    "How-to and style",
+    "Music",
+    "News and politics",
+    "People and blogs",
+    "Pets and animals",
+    "Science and technology",
+    "Sports",
+    "Travel and events",
+];
+
+async function main() {
+    console.log("Sending categories...");
+
+    try {
+        const values = categoryNames.map((name) => ({
+            name,
+            description: `Videos related to ${name.toLowerCase()}`,
+        }));
+
+        await db.insert(categories).values(values);
+
+        console.log("Categories seaded successfully");
+    } catch (error) {
+        console.error("Error seeding categories:", error);
+        process.exit(1);
+    }
+}
+
+main();
