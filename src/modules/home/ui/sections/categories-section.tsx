c8a607bbc9e99b2ce8db88d02bffb4b73d32dbc1
eedu7 <mueedahmad067@gmail.com>
2025-04-10 13:37:49 +0500

    added suspence and error boundary to the categories section

--- src/modules/home/ui/sections/categories-section.tsx
+++ src/modules/home/ui/sections/categories-section.tsx
@@ -1,13 +1,24 @@
 "use client";
 
-import React from "react";
+import React, { Suspense } from "react";
 import { trpc } from "@/trpc/client";
+import { ErrorBoundary } from "react-error-boundary";
 
 interface CategoriesSectionProps {
     categoryId?: string;
 }
 
 export const CategoriesSection = ({ categoryId }: CategoriesSectionProps) => {
+    return (
+        <Suspense fallback={<p>Loading...</p>}>
+            <ErrorBoundary fallback={<p>Error...</p>}>
+                <CategoriesSectionSuspense categoryId={categoryId} />
+            </ErrorBoundary>
+        </Suspense>
+    );
+};
+
+export const CategoriesSectionSuspense = ({ categoryId }: CategoriesSectionProps) => {
     const [categories] = trpc.categories.getMany.useSuspenseQuery();
 
     return <div>{JSON.stringify(categories)}</div>;
