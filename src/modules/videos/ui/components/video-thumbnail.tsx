6bbbfe6f80062778fc1b4ad9ec35f7956443cccc
eedu7 <mueedahmad067@gmail.com>
2025-04-11 21:31:43 +0500

    added preview to the videothumbnail

--- src/modules/videos/ui/components/video-thumbnail.tsx
+++ src/modules/videos/ui/components/video-thumbnail.tsx
@@ -3,18 +3,26 @@ import Image from "next/image";
 
 interface VideoThumbnailProps {
     imageUrl?: string | null;
+    previewUrl?: string | null;
+    title: string;
 }
 
-export const VideoThumbnail = ({ imageUrl }: VideoThumbnailProps) => {
+export const VideoThumbnail = ({ imageUrl, previewUrl, title }: VideoThumbnailProps) => {
     return (
-        <div className="relative">
+        <div className="group relative">
             {/* Thumbnail wrapper */}
             <div className="relative aspect-video w-full overflow-hidden rounded-xl">
                 <Image
                     src={imageUrl ?? "/placeholder.svg"}
-                    alt="Thumbnail"
+                    alt={title}
                     fill
-                    className="size-full object-cover"
+                    className="size-full object-cover group-hover:opacity-0"
+                />
+                <Image
+                    src={previewUrl ?? "/placeholder.svg"}
+                    alt={title}
+                    fill
+                    className="size-full object-cover opacity-0 group-hover:opacity-100"
                 />
             </div>
 
