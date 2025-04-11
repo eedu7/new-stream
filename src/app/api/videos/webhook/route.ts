4ad12b6d217b4d6defd4b6bbebc782a7aa113110
eedu7 <mueedahmad067@gmail.com>
2025-04-11 22:29:08 +0500

    added the video assest track ready

--- src/app/api/videos/webhook/route.ts
+++ src/app/api/videos/webhook/route.ts
@@ -115,8 +115,33 @@ export const POST = async (req: Request) => {
             }
 
             await db.delete(videos).where(eq(videos.muxUploadId, data.upload_id));
+            break;
+        }
+        case "video.asset.track.ready": {
+            const data = payload.data as VideoAssetTrackReadyWebhookEvent["data"] & {
+                asset_id: string;
+            };
+
+            // Typescript incorrectly says that asset id does not exist
+            const assetId = data.asset_id;
+            const trackId = data.id;
+            const status = data.status;
+
+            if (!assetId) {
+                return new Response("Missing asset ID", { status: 400 });
+            }
+
+            await db
+                .update(videos)
+                .set({
+                    muxTrackId: trackId,
+                    muxTrackStatus: status,
+                })
+                .where(eq(videos.muxAssetId, assetId));
+            break;
         }
     }
+
     return new Response("Webhook received", {
         status: 200,
     });
