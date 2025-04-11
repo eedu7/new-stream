b7db09fe12d082ab2674221d192e0d01ccbb78f2
eedu7 <mueedahmad067@gmail.com>
2025-04-11 09:04:28 +0500

    Added the suspence and errorboundary on the videos section

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -1,10 +1,21 @@
 "use client";
 
-import React from "react";
+import React, { Suspense } from "react";
 import { trpc } from "@/trpc/client";
 import { DEFAULT_LIMIT } from "@/constants";
+import { ErrorBoundary } from "react-error-boundary";
 
 export const VideosSection = () => {
+    return (
+        <Suspense fallback={<p>Loading...</p>}>
+            <ErrorBoundary fallback={<p>Error...</p>}>
+                <VideosSectionSuspense />
+            </ErrorBoundary>
+        </Suspense>
+    );
+};
+
+export const VideosSectionSuspense = () => {
     const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery(
         {
             limit: DEFAULT_LIMIT,
