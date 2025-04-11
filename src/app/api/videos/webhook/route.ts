18dedd58b1d1ad8bc39b3275e3cb8660c4ac8917
eedu7 <mueedahmad067@gmail.com>
2025-04-11 21:17:52 +0500

    Added the video assest ready setting and database schem update from the mux webhook

--- src/app/api/videos/webhook/route.ts
+++ src/app/api/videos/webhook/route.ts
@@ -58,6 +58,33 @@ export const POST = async (req: Request) => {
                     muxStatus: data.status,
                 })
                 .where(eq(videos.muxUploadId, data.upload_id));
+            break;
+        }
+        case "video.asset.ready": {
+            const data = payload.data as VideoAssetReadyWebhookEvent["data"];
+            const playbackId = data.playback_ids?.[0].id;
+
+            if (!data.upload_id) {
+                return new Response("Missing upload ID", { status: 400 });
+            }
+
+            if (!playbackId) {
+                return new Response("Missing playback ID", { status: 400 });
+            }
+
+            const thumbnailUrl = `https://image.mux.com/${playbackId}/thumbnail.jpg`;
+
+            await db
+                .update(videos)
+                .set({
+                    muxStatus: data.status,
+                    muxPlaybackId: playbackId,
+                    muxAssetId: data.id,
+                    thumbnailUrl: thumbnailUrl,
+                })
+                .where(eq(videos.muxUploadId, data.upload_id));
+
+            break;
         }
     }
     return new Response("Webhook received", {
