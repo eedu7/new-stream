767fb3343ba58c6174779d6cc99b076d7ea3eaad
eedu7 <mueedahmad067@gmail.com>
2025-04-12 07:16:36 +0500

    Added the form section suspense and error boundary

--- src/modules/studio/ui/views/video-view.tsx
+++ src/modules/studio/ui/views/video-view.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { FormSection } from "@/modules/studio/ui/sections/form-section";
 
 interface VideoViewProps {
     videoId: string;
 }
 
 export const VideoView = ({ videoId }: VideoViewProps) => {
-    return <div className="max-w-screen-lg px-4 pt-2.5">{videoId}</div>;
+    return (
+        <div className="max-w-screen-lg px-4 pt-2.5">
+            <FormSection videoId={videoId} />
+        </div>
+    );
 };
