e7f52dc680d2869ee6d383021794b20bfdf51a6d
eedu7 <mueedahmad067@gmail.com>
2025-04-12 08:08:30 +0500

    Added the form and added the field for the title

--- src/modules/studio/ui/sections/form-section.tsx
+++ src/modules/studio/ui/sections/form-section.tsx
@@ -12,6 +12,11 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { MoreVerticalIcon, TrashIcon } from "lucide-react";
 import { useForm } from "react-hook-form";
+import { videoUpdateSchema } from "@/db/schema";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 interface FormSectionProps {
     videoId: string;
@@ -34,42 +39,71 @@ const FormSectionSkeleton = () => {
 export const FormSectionSuspense = ({ videoId }: FormSectionProps) => {
     const [video] = trpc.studio.getOne.useSuspenseQuery({ id: videoId });
 
-    const form = useForm({
-        defaultValue: video,
+    const form = useForm<z.infer<typeof videoUpdateSchema>>({
+        resolver: zodResolver(videoUpdateSchema),
+        defaultValues: video,
     });
 
+    const onSubmit = async (data: z.infer<typeof videoUpdateSchema>) => {
+        console.log(data);
+    };
+
     return (
-        <div className="mb-6 flex w-full items-center justify-between">
-            <div>
-                <h1 className="text-2xl font-bold">Video details</h1>
-                <p className="text-muted-foreground text-xs">Manage your video details</p>
-            </div>
-            <div className="flex items-center gap-x-2">
-                <Button
-                    disabled={false}
-                    className="cursor-pointer"
-                    type="submit"
-                >
-                    Save
-                </Button>
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+                <div className="mb-6 flex w-full items-center justify-between">
+                    <div>
+                        <h1 className="text-2xl font-bold">Video details</h1>
+                        <p className="text-muted-foreground text-xs">Manage your video details</p>
+                    </div>
+                    <div className="flex items-center gap-x-2">
                         <Button
+                            disabled={false}
                             className="cursor-pointer"
-                            variant="ghost"
-                            size="icon"
+                            type="submit"
                         >
-                            <MoreVerticalIcon />
+                            Save
                         </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        <DropdownMenuItem className="flex cursor-pointer items-center">
-                            <TrashIcon className="mr-2 size-4" />
-                            Delete
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            </div>
-        </div>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <Button
+                                    className="cursor-pointer"
+                                    variant="ghost"
+                                    size="icon"
+                                >
+                                    <MoreVerticalIcon />
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end">
+                                <DropdownMenuItem className="flex cursor-pointer items-center">
+                                    <TrashIcon className="mr-2 size-4" />
+                                    Delete
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </div>
+                </div>
+                <div className="grid grid-cols-1 gap-6 lg:grid-cols-5">
+                    <div className="space-y-8 lg:col-span-3">
+                        <FormField
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel></FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            placeholder="Add a title to your video"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                            name="title"
+                        />
+                    </div>
+                </div>
+            </form>
+        </Form>
     );
 };
