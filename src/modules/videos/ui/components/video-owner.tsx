c4bdadfb5c8e6f8b1eec95f98c8799f0f26858d2
eedu7 <mueedahmad067@gmail.com>
2025-04-18 20:32:38 +0500

    Some changes

--- src/modules/videos/ui/components/video-owner.tsx
+++ src/modules/videos/ui/components/video-owner.tsx
@@ -1,5 +1,41 @@
 import React from "react";
+import { VideoGetOneOutput } from "@/modules/videos/types";
+import Link from "next/link";
+import { UserAvatar } from "@/components/user-avatar";
+import { useAuth } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
 
-export const VideoOwner = () => {
-    return <div>VideoOwner</div>;
+interface VideoOwnerProps {
+    user: VideoGetOneOutput["user"];
+    videoId: string;
+}
+
+export const VideoOwner = ({ videoId, user }: VideoOwnerProps) => {
+    const { userId } = useAuth();
+
+    return (
+        <div className="ggap-3 flex min-w-0 items-center justify-between sm:items-start sm:justify-start">
+            <Link href={`/users/${user.id}`}>
+                <div className="flex min-w-0 items-center gap-3">
+                    <UserAvatar
+                        name={user.name}
+                        imageUrl={user.imageUrl}
+                        size="lg"
+                    />
+                    {/* Properly fill user count */}
+                    <span className="text-muted-foreground line-clamp-1 text-sm">{0} subscribers</span>
+                </div>
+            </Link>
+            {userId === user.clerkI ? (
+                <Button
+                    className="rounded-full"
+                    variant="secondary"
+                >
+                    <Link href={`/studio/videos/${videoId}`}>Edit video</Link>
+                </Button>
+            ) : (
+                <SubscriptionButton />
+            )}
+        </div>
+    );
 };
--- src/modules/videos/ui/components/video-top-row.tsx
+++ src/modules/videos/ui/components/video-top-row.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { VideoGetOneOutput } from "@/modules/videos/types";
+import { VideoOwner } from "@/modules/videos/ui/components/video-owner";
 
 interface VideoTopRowProps {
     video: VideoGetOneOutput;
@@ -10,7 +11,10 @@ export const VideoTopRow = ({ video }: VideoTopRowProps) => {
         <div className="mt-4 flex flex-col gap-4">
             <h1 className="text-xl font-semibold">{video.title}</h1>
             <div className="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
-                {/*<VideoOwner user={video.user?} videoId={video.videoId} />*/}
+                <VideoOwner
+                    user={video.user}
+                    videoId={video.id}
+                />
             </div>
         </div>
     );
