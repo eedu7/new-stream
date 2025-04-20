c512f64d70ce4de19e9eb2569c31fc1966c767ea
eedu7 <mueedahmad067@gmail.com>
2025-04-20 20:53:49 +0500

    Added the videoViews create protectedProedure

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -4,11 +4,13 @@ import { createTRPCRouter } from "@/trpc/init";
 import { categoriesRouter } from "@/modules/categories/server/procedures";
 import { studioRouter } from "@/modules/studio/server/procedures";
 import { videosRouter } from "@/modules/videos/server/prodedures";
+import { videoViewsRouter } from "@/modules/video-views/server/procedure";
 
 export const appRouter = createTRPCRouter({
     categories: categoriesRouter,
     studio: studioRouter,
     videos: videosRouter,
+    videoViews: videoViewsRouter,
 });
 
 export type AppRouter = typeof appRouter;
