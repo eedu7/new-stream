dbdea199bb2bb4e54ce92ff066905e1354f21764
eedu7 <mueedahmad067@gmail.com>
2025-04-20 22:17:30 +0500

    Added the like and dislike procedure

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -5,12 +5,14 @@ import { categoriesRouter } from "@/modules/categories/server/procedures";
 import { studioRouter } from "@/modules/studio/server/procedures";
 import { videosRouter } from "@/modules/videos/server/prodedures";
 import { videoViewsRouter } from "@/modules/video-views/server/procedure";
+import { videoReactionsRouter } from "@/modules/video-reactions/server/procedure";
 
 export const appRouter = createTRPCRouter({
     categories: categoriesRouter,
     studio: studioRouter,
     videos: videosRouter,
     videoViews: videoViewsRouter,
+    videoReactions: videoReactionsRouter,
 });
 
 export type AppRouter = typeof appRouter;
